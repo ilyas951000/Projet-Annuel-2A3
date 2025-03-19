2cb61e8cb53e70dcc3365ceacca31ae2c9dbc6a5
sangeric <eric.sang.pro@gmail.com>
2025-03-19 16:19:05 +0100

    front en nextJs de l'inscription

--- Ecodeli/backend-eco-deli/src/main.ts
+++ Ecodeli/backend-eco-deli/src/main.ts
@@ -9,6 +9,6 @@ async function bootstrap() {
   // Servir les fichiers statiques depuis le dossier "public"
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.enableCors();
-  await app.listen(3000);
+  await app.listen(3001);
 }
 bootstrap();
