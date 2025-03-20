05c5a992b7dd98e7d90da166fdc23be65227b1d4
sangeric <eric.sang.pro@gmail.com>
2025-03-20 18:15:55 +0100

    structure for the connexion of an user if it is admin prestataire client etc ..

---
+++ Ecodeli/frontend-eco-deli/src/app/dashboard/client/page.tsx
@@ -0,0 +1,12 @@
+// app/dashboard/admin/page.tsx
+import type { NextPage } from 'next';
+
+const AdminConnexion: NextPage = () => {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-3xl font-bold">Voici la connexion client</h1>
+    </div>
+  );
+};
+
+export default AdminConnexion;
