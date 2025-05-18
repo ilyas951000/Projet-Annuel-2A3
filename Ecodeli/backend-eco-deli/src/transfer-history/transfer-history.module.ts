026bf79be291ea786cc5d2bf0fffc9adf61e35e9
ilyas <ilyas.salek@gmail.com>
2025-05-18 18:43:29 +0200

    new version parfaite du 18/05

--- Ecodeli/backend-eco-deli/src/transfer-history/transfer-history.module.ts
+++ Ecodeli/backend-eco-deli/src/transfer-history/transfer-history.module.ts
@@ -1,14 +1,13 @@
-// src/transfer-history/transfer-history.module.ts
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TransferHistory } from './entities/transfer-history.entity';
-import { Localisation } from 'src/localisation/entities/localisation.entity';
 import { TransferHistoryService } from './transfer-history.service';
 import { TransferHistoryController } from './transfer-history.controller';
+import { Localisation } from 'src/localisation/entities/localisation.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([TransferHistory, Localisation])],
   providers: [TransferHistoryService],
-  controllers: [TransferHistoryController],
+  controllers: [TransferHistoryController], // ← OBLIGATOIRE
 })
 export class TransferHistoryModule {}
